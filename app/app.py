77975ce95f95daaed576e01dcdd0827003290aa9
karasu126 <luqmanmuhammad2005@gmail.com>
2026-03-22 22:18:27 +0700

    initial commit: flask app and dockerfile

---
+++ app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, app, jsonify
+
+myapp  = Flask(__name__)
+
+@myapp.route('/', methods=['GET'])
+def welcome():
+    return 'Hello world, my name is Luqman'
+
+@myapp.route('/health', methods=['GET'])
+def health():
+    data = {
+        "status": "ok"
+        }
+    return jsonify(data)
+
+if __name__ == '__main__':
+    myapp.run(host='0.0.0.0', port=5000, debug=False)
