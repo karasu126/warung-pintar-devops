d572989bcfc8e94c3f47f3b8f16da12cf5d63f7d
karasu126 <luqmanmuhammad2005@gmail.com>
2026-03-24 06:48:00 +0700

    update welcome message to highlight areas for improvement in the project

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ myapp  = Flask(__name__)
 
 @myapp.route('/', methods=['GET'])
 def welcome():
-    return 'Hello world, my name is Luqman, this project is a simple flask app. I am learning how to build CI/CD pipelines with github actions and docker. I hope you find this project useful.'
+    return 'Hello world, my name is Luqman, this project is a simple flask app. I am learning how to build CI/CD pipelines with github actions and docker. I hope you find this project useful. There is so much part of this project that can be improved.'
 
 @myapp.route('/health', methods=['GET'])
 def health():
