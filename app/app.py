7c95de11de85d5dc014dfe3401147ae2fa2520f0
karasu126 <luqmanmuhammad2005@gmail.com>
2026-03-27 13:37:23 +0700

    update welcome message to test rollback

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ myapp  = Flask(__name__)
 
 @myapp.route('/', methods=['GET'])
 def welcome():
-    return 'Hello world, my name is Luqman, this project is a simple flask app. I am learning how to build CI/CD pipelines with github actions and docker. I hope you find this project useful. There is so much part of this project that can be improved. This is a simple Flask app with rollback functionality (branch improve/v2).'
+    return 'Hello world, my name is Luqman, this project is a simple flask app. I am learning how to build CI/CD pipelines with github actions and docker. I hope you find this project useful. There is so much part of this project that can be improved. This is a simple Flask app with rollback functionality (branch improve/v2). This is latest version of the app (want to test rollback mechanism).'
 
 @myapp.route('/health', methods=['GET'])
 def health():
